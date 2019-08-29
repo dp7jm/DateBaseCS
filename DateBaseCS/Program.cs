fef37a23577a93651c6d46ef992dfa7a7d41020f
{dp7jm} <{dpjm@chef.net}>
2019-08-29 04:04:37 +0200

    Add project files.

---
+++ DateBaseCS/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateBaseCS
+{
+    class DbConnection
+    {
+        public string ConectionString { get; set; }
+        public TimeSpan Timneout { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
